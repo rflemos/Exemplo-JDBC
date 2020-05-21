486e416aff11a447a142df73e6b90d45c7546eba
Rafael <rafafaine@gmail.com>
2020-05-21 16:18:11 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
 		System.out.println("=== TEST 1: seller  findById ===");
@@ -47,7 +48,12 @@ public class Program {
 		
 		System.out.println("Update Completed");
 		
-		
+		System.out.println("\n=== TEST 5: seller  update  ===");
+		System.out.println("Enter if for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleted Completed ");
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -91,8 +91,21 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public void deleteById(Seller id) {
-		
+	public void deleteById(Integer id) {
+		PreparedStatement st = null;
+		try {
+			st =conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			st.setInt(1, id);
+			
+			st.execute();
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
